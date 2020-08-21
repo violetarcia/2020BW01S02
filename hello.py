e287ff02a96d9a959dd82fb1e24ac93d98a03495
rhdzmota <rhdzmota@gmail.com>
2020-08-20 21:45:32 -0500

    Added french implementation

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
 import sys
 
+def french():
+    print("Bonjour")
+
 def english():
     print("Hello")
 
 
 def main():
-    english()
+    if sys.argv[1] == "f":
+        french()
+    else:
+        english()
 
 
 if __name__ == "__main__":
