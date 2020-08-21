c2a66555a5000efa04cdd565551196b9b36bb815
rhdzmota <rhdzmota@gmail.com>
2020-08-20 21:41:50 -0500

    Added spanish function

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 import sys
 
+
+def spanish():
+    print("Hola")
+
 def english():
     print("Hello")
 
 
 def main():
-    english()
+    if sys.argv[1] == "s":
+        spanish()
+    else:
+        english()
 
 
 if __name__ == "__main__":
